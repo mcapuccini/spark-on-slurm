99c4045e257f3618b145225fedc0a81ca7d79ce4
laeeq80 <laeeq@kth.se>
2015-08-28 15:56:33 +0200

    modified start-cluster.sh to start slave process on master node

--- start-cluster.sh
+++ start-cluster.sh
@@ -3,7 +3,8 @@
 MASTER=$1
 HOST=$(hostname)
 if [ "$HOST" == "$MASTER" ]; then
-	$SPARK_HOME/sbin/start-master.sh 
+	$SPARK_HOME/sbin/start-master.sh
+	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 
 else
 	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 
 fi 
