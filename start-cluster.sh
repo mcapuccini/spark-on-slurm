3e25627624ed309549e3fb2dbda1fceedabd1fc3
Marco Capuccini <marcoca@milou1.uppmax.uu.se>
2015-08-29 15:52:05 +0200

    executor on master node uses 2 cores less now

--- start-cluster.sh
+++ start-cluster.sh
@@ -3,8 +3,9 @@
 MASTER=$1
 HOST=$(hostname)
 if [ "$HOST" == "$MASTER" ]; then
-	$SPARK_HOME/sbin/start-master.sh
-	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 
+	N_CORES=$(grep -c ^processor /proc/cpuinfo)
+	$SPARK_HOME/sbin/start-master.sh 
+	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 --cores $((N_CORES-2)) 
 else
 	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 
 fi 
