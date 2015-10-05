f1b5e77a051a98c76001194c462a19210f1c5f6a
Marco Capuccini <marcoca@milou2.uppmax.uu.se>
2015-10-05 10:58:45 +0200

    same number of cpu on master node

--- start-cluster.sh
+++ start-cluster.sh
@@ -3,12 +3,10 @@
 MASTER=$1
 HOST=$(hostname)
 if [ "$HOST" == "$MASTER" ]; then
-	N_CORES=$(grep -c ^processor /proc/cpuinfo)
 	$SPARK_HOME/sbin/start-master.sh 
-	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 --cores $((N_CORES-2)) 
-else
-	$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077 
-fi 
+fi
+
+$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077  
 
 tail -f /dev/null #wait forever
 
